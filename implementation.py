c81ec67e4c7b80dd564d1b9764c003487e0034b7
Dylan Harris <dharris@linkedin.com>
2018-05-11 13:23:58 -0700

    Pass test 1

--- implementation.py
+++ implementation.py
@@ -1,2 +1,7 @@
-def alive_rule(cell_num, num_alive_neighbors):
-    return num_alive_neighbors >= 2 if cell_num else num_alive_neighbors == 3
+class Life:
+
+    def __init__(self):
+        self.board = []
+
+    def alive_rule(self, cell_num, num_alive_neighbors):
+        return num_alive_neighbors in [2,3] or num_alive_neighbors > 4 if cell_num else num_alive_neighbors == 3 or num_alive_neighbors > 4
