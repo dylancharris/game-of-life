929f4d6f220cedffee82be75a645b5bde3cdd27f
Jeff Rogers <jerogers@linkedin.com>
2018-05-11 13:35:04 -0700

    output formatting

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ from implementation import Life
 
 l = Life()
 
+print("Alive rule")
 assert l.alive_rule(1, 2)
 assert l.alive_rule(1, 3)
 assert l.alive_rule(0, 3)
@@ -21,6 +22,7 @@ assert not l.alive_rule(1, 4)
 # board initially all dead
 # dead = False, alive = True
 
+print("get/set/clear")
 assert not l.get(0,0)
 assert not l.get(1,4)
 assert not l.get(3,3)
@@ -43,7 +45,29 @@ assert l.get(1,1)
 
 # count neighbors
 # Live.count(x,y) = the number of live neighbors
+print("count neighbors")
 assert l.count(0,0) == 1
 assert l.count(1,1) == 1
 assert l.count(0,1) == 2
 assert l.count(2,2) == 2
+
+# output formatting:
+# return a list of lines where a dead cell is formatted as " " and 
+# a living cell is formatted as "#"
+assert l.format() == [
+ "#   ",
+ " #  ",
+ "    ",
+ "   #"]
+l.clear(0,0)
+assert l.format() == [
+ "    ",
+ " #  ",
+ "    ",
+ "   #"]
+l.set(2,0)
+assert l.format() == [
+ "  # ",
+ " #  ",
+ "    ",
+ "   #"]
